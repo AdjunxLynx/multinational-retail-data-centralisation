af53b90b01f8a86e900ccd6f0d2cc7cac404c0c7
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-12-09 21:59:23 +0000

    updates database utils to connect to cloud DB. updates data_extraction to list all DB tables. extract_rds_tables and loop still not working

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,28 @@
-class DataExtractor:
-    pass
+from database_utils import DatabaseConnector as DBC
+from sqlalchemy import text
+import pandas as pd
+class DataExtractor():
+    def list_db_tables(self):
+        e = DBC
+        self.engine = DBC.init_db_engine(e)
+        with self.engine.connect() as connection:
+            result = connection.execute(text("SELECT table_name"
+                " FROM INFORMATION_SCHEMA.TABLES"
+                " WHERE table_type = 'BASE TABLE'"))
+            print(result)
+            return result
+    def extract_rds_table(self, table):
+        table = self.list_db_tables()
+        df = pd.DataFrame(data=tables)
+        print(df)
+    
+    def loop(self):
+        df = pd.DataFrame(data = [])
+        tables = self.list_db_tables()
+        for i in self.list_db_tables():
+            df.append(self.extract_rds_table(table = tables[i]))
+        
+        return df
+        
+db = DataExtractor()
+db.loop()
--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,17 @@
-class DatabaseConnector:
-    pass
+import sqlalchemy as sqla
+from sqlalchemy import text
+import yaml
+from yaml.loader import SafeLoader
+class DatabaseConnector():
+    def read_db_creds(self):
+        with open('db_creds.yaml') as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            return data
+    def init_db_engine(self):
+            creds = self.read_db_creds(self)
+            url = ("postgresql+psycopg2://" + creds["RDS_USER"] + ":" + creds["RDS_PASSWORD"] + "@" + creds["RDS_HOST"] + ":5432/" + creds["RDS_DATABASE"])
+            print(url)
+            engine = sqla.create_engine(url)
+            engine
+            return engine
+    
