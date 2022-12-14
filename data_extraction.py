d31442df711a5a582de3a8d6a62012739709018b
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-12-14 17:33:12 +0000

    adds functionality to get table names and tries to place them in pandas dataframe(not working yet)

--- data_extraction.py
+++ data_extraction.py
@@ -1,28 +1,50 @@
 from database_utils import DatabaseConnector as DBC
-from sqlalchemy import text
+
+import sqlalchemy
+from sqlalchemy import inspect
+from sqlalchemy.ext.declarative import declarative_base
 import pandas as pd
 class DataExtractor():
     def list_db_tables(self):
-        e = DBC
-        self.engine = DBC.init_db_engine(e)
+        pd.set_option('display.max_rows', 1000)
+        e = DBC()
+        self.engine = e.init_db_engine()
+        inspector = inspect(self.engine)
+        schemas = inspector.get_schema_names()
+        tables = []
+        for schema in schemas:
+            for table_name in inspector.get_table_names(schema=schema):
+                for column in inspector.get_columns(table_name, schema=schema):
+                    try:
+                            str(int(column["name"]))
+                    except:
+                        tables.append(str(column["name"]))
+        return tables
+        
+    def extract_rds_table(self, DBC, table):
+        Base = declarative_base()
         with self.engine.connect() as connection:
-            result = connection.execute(text("SELECT table_name"
-                " FROM INFORMATION_SCHEMA.TABLES"
-                " WHERE table_type = 'BASE TABLE'"))
-            print(result)
-            return result
-    def extract_rds_table(self, table):
-        table = self.list_db_tables()
-        df = pd.DataFrame(data=tables)
-        print(df)
-    
-    def loop(self):
-        df = pd.DataFrame(data = [])
-        tables = self.list_db_tables()
-        for i in self.list_db_tables():
-            df.append(self.extract_rds_table(table = tables[i]))
-        
-        return df
-        
+            table_str = ""
+            table = table
+            for i in range(len(table)):
+                table_str = ", ".join(table)
+            print("here")
+            print(table_str)
+            print("here")
+            result = connection.execute(f"SELECT * FROM {table_str}")
+            column = result.keys()
+            keys = column
+            keys_joined = ""
+            for i in keys:
+                keys_joined.join(", " + str(i))
+            query = f'SELECT {keys_joined} FROM {table}'
+            
+            result = connection.execute(query)
+            info = []
+            for element in result:
+                info.append(element)
+        column = sqlalchemy.engine.Result.keys()
+        df = pd.DataFrame(data=info, columns=column)
 db = DataExtractor()
-db.loop()
+e = DBC
+db.extract_rds_table(e, db.list_db_tables())
--- database_utils.py
+++ database_utils.py
@@ -3,15 +3,16 @@ from sqlalchemy import text
 import yaml
 from yaml.loader import SafeLoader
 class DatabaseConnector():
+    def __init__(self) -> None:
+         pass
+     
     def read_db_creds(self):
         with open('db_creds.yaml') as f:
             data = yaml.load(f, Loader=SafeLoader)
             return data
     def init_db_engine(self):
-            creds = self.read_db_creds(self)
-            url = ("postgresql+psycopg2://" + creds["RDS_USER"] + ":" + creds["RDS_PASSWORD"] + "@" + creds["RDS_HOST"] + ":5432/" + creds["RDS_DATABASE"])
+            creds = self.read_db_creds()
+            url = ("postgresql+psycopg2://" + creds["RDS_USER"] + ":" + creds["RDS_PASSWORD"] + "@" + creds["RDS_HOST"] + ":" + str(creds["RDS_PORT"]) + "/" + creds["RDS_DATABASE"])
             print(url)
             engine = sqla.create_engine(url)
-            engine
             return engine
-    
