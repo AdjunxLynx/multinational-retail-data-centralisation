92499e76d371c95e6df2483555c4396748c00f90
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-12-14 18:49:16 +0000

    returns all tables and data within those tables with correct column names

--- data_extraction.py
+++ data_extraction.py
@@ -1,50 +1,38 @@
 from database_utils import DatabaseConnector as DBC
 
 import sqlalchemy
-from sqlalchemy import inspect
+from sqlalchemy import inspect, text
 from sqlalchemy.ext.declarative import declarative_base
 import pandas as pd
 class DataExtractor():
-    def list_db_tables(self):
-        pd.set_option('display.max_rows', 1000)
-        e = DBC()
-        self.engine = e.init_db_engine()
-        inspector = inspect(self.engine)
+    def list_db_tables(self, engine):
+        inspector = inspect(engine)
         schemas = inspector.get_schema_names()
+
         tables = []
         for schema in schemas:
-            for table_name in inspector.get_table_names(schema=schema):
-                for column in inspector.get_columns(table_name, schema=schema):
-                    try:
-                            str(int(column["name"]))
-                    except:
-                        tables.append(str(column["name"]))
+            for table_name in inspector.get_table_names("public"):
+                tables.append(table_name)
+        print(tables)
         return tables
+    
+    def read_table_data(self, table_name, engine):
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM " + table_name))
+        return result, result.keys()
+
+    def extract_rds_table(self, engine, table_name):
+        pass
+        column = self.read_table_data(table_name, engine)[1]
+        df = pd.DataFrame(self.read_table_data(table_name, engine)[0], columns = column)
+        print(df)
+        return df
         
-    def extract_rds_table(self, DBC, table):
-        Base = declarative_base()
-        with self.engine.connect() as connection:
-            table_str = ""
-            table = table
-            for i in range(len(table)):
-                table_str = ", ".join(table)
-            print("here")
-            print(table_str)
-            print("here")
-            result = connection.execute(f"SELECT * FROM {table_str}")
-            column = result.keys()
-            keys = column
-            keys_joined = ""
-            for i in keys:
-                keys_joined.join(", " + str(i))
-            query = f'SELECT {keys_joined} FROM {table}'
-            
-            result = connection.execute(query)
-            info = []
-            for element in result:
-                info.append(element)
-        column = sqlalchemy.engine.Result.keys()
-        df = pd.DataFrame(data=info, columns=column)
-db = DataExtractor()
-e = DBC
-db.extract_rds_table(e, db.list_db_tables())
+        
+    def run(self, engine):
+        for i in self.list_db_tables(engine):
+            self.extract_rds_table(engine, i)
+        pass
+a =DataExtractor()
+DBC = DBC()
+a.run(DBC.init_db_engine())
